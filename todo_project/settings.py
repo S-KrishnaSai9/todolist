29c5afd5f6ae39ae09b686c57003e3708c2bdee1
S-KrishnaSai9 <68336455+S-KrishnaSai9@users.noreply.github.com>
2022-06-15 15:20:29 +0530

    Update settings.py

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -1,5 +1,3 @@
-import django_heroku
-
 """
 Django settings for todo_project project.
 
@@ -124,5 +122,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-django_heroku.settings(locals())
